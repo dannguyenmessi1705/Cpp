94dbc61856afbea13026466ae6c02e854e6f2ef9
dannguyenmessi1705 <messiprohy@gmail.com>
2022-10-05 07:29:55 +0700

    Cpp

--- DEC2BIN_ThapPhanSangNhiPhan.cpp
+++ DEC2BIN_ThapPhanSangNhiPhan.cpp
@@ -1,20 +1,24 @@
-
 #include <bits/stdc++.h>
-#define ll long long
+typedef unsigned long long int lli;
 using namespace std;
 
 int t;
-ll n;
+lli n;
 stack<int>st;
 int main() {
-ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-  cin >> t;
-  while(t--){
-    cin >> n;
-    while(n!=0) st.push(n%2), n/=2;
-    while(!st.empty()) {cout<<st.top(); st.pop();}
-    cout<<'\n';
-  }
+    cin >> t;
+    while(t--){
+      cin >> n;
+      while(n!=0){ 
+          st.push(n%2); // Them gia tri vao ngan xep tu duoi len tren
+          n/=2;
+      }
+      while(!st.empty()) {
+        cout<<st.top(); // Lay gia tri dau tien cua ngan xep
+        st.pop(); // Loai bo gia tri dau cua ngan xep
+      }
+      cout<<'\n';
+    }
 
-return 0;
+  return 0;
 }
