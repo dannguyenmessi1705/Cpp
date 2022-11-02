00e20fef13942cd37bf54bb63d29f567664e68d2
dannguyenmessi1705 <messiprohy@gmail.com>
2022-11-02 22:08:24 +0700

    Cpp

--- COBAN002_SoChinhPhuong.cpp
+++ COBAN002_SoChinhPhuong.cpp
@@ -1,51 +1,36 @@
-#include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-typedef unsigned long long int lli;
-int Kiemtra(lli number){
-    if(number == 0) return 1;
-    double test;
-    test = sqrt(number);
-    if(test == (int)test) return 1;
-    return 0;
-}
-int TachKiemTra(lli n){
-    if(n>=0){
-        while(n>0){
-            lli a = n%10;
-            if(Kiemtra(a)==0) return 0;
-            n/=10;
-        }
-        return 1;
-    }
-    return 0;
-}
-lli SoCP(int n){
-    if(n<2) return 0;
-    else{
-        lli a=pow(10,n-1);
-        lli b=pow(10,n)-1;
-        lli begin;
-        if(Kiemtra(a)==0) 
-            begin=(int)sqrt(a)+1;
-        else 
-            begin=sqrt(a);
-        for(lli i=begin;i<=(int)sqrt(b);i++){
-            lli ktra = 0;
-            ktra = i*i;
-            if(TachKiemTra(ktra)==1) return ktra;
-        }
-    }
-    return 0;
 
-
+bool check(ll n){
+    if(n == 0) return true;
+    ll x = sqrt(n);
+    if(x*x == n) return true;
+    return false;
 }
-int main(){
-    int test;
-    cin>>test;
-    while(test--){
-        int n;
-        cin>>n;
-        cout<<SoCP(n)<<endl;
+bool check1(ll n){
+    while(n!=0){
+        ll x = n%10;
+        if(x!=0 && x != 1 && x!=4 && x!=9) return false;
+        n/=10;
     }
+    return true;
+}
+
+int main (){
+    int t; cin >>t;
+    while(t--){
+        ll n;
+        cin >>n;
+        ll low = pow(10,n-1);
+        ll high = pow(10,n)-1;
+        for(ll i = low; i <= high; ++i){
+            if(check(i) && check1(i)){
+                cout <<i;
+                break;
+            }
+        }
+        cout <<'\n';
+    }
+    return 0;
 }
--- GTBT001_DaThucTong.cpp
+++ GTBT001_DaThucTong.cpp
@@ -1,17 +1,14 @@
-#include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
-float Tong(double x, int n){
+float Tong(float x, int n){
     float result = 0; // KQ phải ra float bài mới đúng :))
-    long double tu = 1;
+    float tu = 1;
     int mau = 0;
     for(int i=1;i<=n;i++){
         tu *= x;
         mau += i;
         result += tu/mau;
     }
-    result = trunc(1000*result)/1000; // lấy 3 số dsau dấu phẩy mà không làm tròn
     return result;
 
 }
@@ -20,7 +17,7 @@ int main(){
     cin>>test;
     while(test--){
         int n;
-        long double x;
+        float x;
         cin>>n>>x;
         cout<<fixed<<setprecision(3)<<Tong(x,n)<<endl;
     }
