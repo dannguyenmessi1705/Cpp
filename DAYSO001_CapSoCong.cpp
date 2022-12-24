0b6d080db0457fac47d240579eb1b02477acd9a5
dannguyenmessi1705 <messiprohy@gmail.com>
2022-12-24 11:28:54 +0700

    Cpp

--- DAYSO001_CapSoCong.cpp
+++ DAYSO001_CapSoCong.cpp
@@ -1,3 +1,8 @@
+/*Dãy cấp số cộng là một dãy số thoả mãn điều kiện: hai phần tử liên tiếp nhau sai khác nhau một hằng số (gọi là công sai của cấp số cộng).
+
+Ví dụ: Dãy số: 3, 5, 7, 9, 11, ...3,5,7,9,11,... là một cấp số cộng với các phân tử liên tiếp sai khác nhau với công sai là 2.
+
+Cho một dãy số nguyên aa gồm nn phần tử (1≤n≤10^6)(1≤n≤10). Hãy kiểm tra xem dãy đó có phải là dãy cấp số cộng hay không?*/
 #include <iostream>
 using namespace std;
 typedef long long int ll;
